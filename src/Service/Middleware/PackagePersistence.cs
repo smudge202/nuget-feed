4ce9122868eac32fbec2a3e075c530bd344444cb
Matt Ridgway <matt.ridgway@live.co.uk>
2015-04-11 15:12:33 +0100

    First passing test - check the context is not null

---
+++ src/Service/Middleware/PackagePersistence.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Threading.Tasks;
+
+namespace NuGet.Feed.Service.Middleware
+{
+    public class PackagePersistence : OwinMiddleware
+    {
+        public PackagePersistence(OwinMiddleware next) : base(next) { }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            return Next.Invoke(context);
+        }
+    }
+}
---
+++ tests/Service.Tests/Middleware/PackagePersistenceTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NuGet.Feed.Service.Middleware;
+using System;
+using Xunit;
+
+namespace NuGet.Feed.Service.Tests.Middleware
+{
+    public class PackagePersistenceTests
+    {
+        [Fact]
+        public void GivenNullContext_WhenInvoked_ThenThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => { new PackagePersistence(null).Invoke(null); })
+                .ParamName.Should().Be("context");
+        }
+    }
+}
