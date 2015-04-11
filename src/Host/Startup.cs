ed8d9c9b3c8ad0abb4a95074549d7a786819f560
Matt Ridgway <matt.ridgway@live.co.uk>
2015-04-11 12:32:50 +0100

    added owin.diagnostics to make debugging easier later

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -1,6 +1,12 @@
-﻿namespace NuGet.Feed.Host
+﻿using Owin;
+
+namespace NuGet.Feed.Host
 {
     public class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+        }
     }
 }
