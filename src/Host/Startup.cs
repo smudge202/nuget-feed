8d94bfdfd3fe7c8db91899ab7ffb506e71a64544
Matt Ridgway <matt.ridgway@live.co.uk>
2015-04-11 13:18:04 +0100

    Added simple Map branch in startup as an example for discussion

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -5,10 +5,26 @@ namespace NuGet.Feed.Host
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration(IAppBuilder builder)
         {
-            app.UseErrorPage();
-            app.Use<AddApiKeyCheckMiddleware>();
+            builder.UseErrorPage();
+
+            builder.MapWhen(m => m.Request.Method == "PUT", app =>
+            {
+                app.Use<AddApiKeyCheckMiddleware>();
+
+                app.Run(context =>
+                {
+                    context.Response.ContentType = "text/plain";
+                    return context.Response.WriteAsync("PUT something");
+                });
+            });
+
+            builder.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
 
         }
     }
